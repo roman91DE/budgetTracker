1892c3bfc5c95324bbcccf899a0d8c78da0f8c7e
Roman Hoehn <rohoehn123@gmail.com>
2024-03-13 18:40:04 +0100

    implemented basic user struct and test

--- models/user.go
+++ models/user.go
@@ -1,27 +1,42 @@
 package models
 
 import (
-    "golang.org/x/crypto/bcrypt"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-    ID       uint   `json:"id" gorm:"primary_key"`
-    Email    string `json:"email" gorm:"type:varchar(100);unique_index"`
-    Password string `json:"password"`
+	ID       uint   `json:"id" gorm:"primary_key"`
+	Email    string `json:"email" gorm:"type:varchar(100);unique_index"`
+	Password string `json:"password"`
+}
+
+func makeUser(id uint, email, passwd string) (*User, error) {
+
+	if len([]byte(passwd)) > 70 {
+		return nil, fmt.Errorf("password is too long! Cannot create user")
+	}
+	user := &User{
+		ID:       id,
+		Email:    email,
+		Password: passwd,
+	}
+
+	return user, nil
 }
 
 // HashPassword generates a hashed password from a plaintext string
-func (u *User) HashPassword(password string) error {
-    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-    if err != nil {
-        return err
-    }
-    u.Password = string(bytes)
-    return nil
+func (u *User) HashPassword() error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(u.Password), 14)
+	if err != nil {
+		return err
+	}
+	u.Password = string(bytes)
+	return nil
 }
 
 // CheckPassword compares a plaintext password with the user's hashed password
 func (u *User) CheckPassword(password string) bool {
-    err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
-    return err == nil
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
 }
--- models/user_test.go
+++ models/user_test.go
@@ -2,26 +2,67 @@
 
 package models
 
-import "testing"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"testing"
+)
 
 // TestUserInitialization tests the initialization of a User struct.
 func TestUserInitialization(t *testing.T) {
-    user := User{
-        ID:       1,
-        Email:    "test@example.com",
-        Password: "password123",
-    }
+	user, err := makeUser(1, "test@example.com", "password123")
 
-    // Check if the User struct is initialized with the correct values.
-    if user.ID != 1 {
-        t.Errorf("Expected ID to be '1', got %v", user.ID)
-    }
-    if user.Email != "test@example.com" {
-        t.Errorf("Expected Email to be 'test@example.com', got %v", user.Email)
-    }
-    if user.Password != "password123" {
-        t.Errorf("Expected Password to be 'password123', got %v", user.Password)
-    }
+	if err != nil {
+		t.Errorf("makeUser failed on valid input: %v", err)
+	}
+
+	// Check if the User struct is initialized with the correct values.
+	if user.ID != 1 {
+		t.Errorf("Expected ID to be '1', got %v", user.ID)
+	}
+	if user.Email != "test@example.com" {
+		t.Errorf("Expected Email to be 'test@example.com', got %v", user.Email)
+	}
+	if user.Password != "password123" {
+		t.Errorf("Expected Password to be 'password123', got %v", user.Password)
+	}
 }
 
-func
+func TestPasswordHashing(t *testing.T) {
+
+	plaintextPassword := "password123"
+	user := User{
+		ID:       1,
+		Email:    "test@example.com",
+		Password: plaintextPassword,
+	}
+
+	user.HashPassword()
+
+	if user.Password == plaintextPassword {
+		t.Errorf("Expected hashed Password but still the same as Plaintext Argument")
+	}
+}
+
+func TestPasswordChecks(t *testing.T) {
+
+	plaintextPassword := "password123"
+	user := User{
+		ID:       1,
+		Email:    "test@example.com",
+		Password: plaintextPassword,
+	}
+
+	user.HashPassword()
+
+	falsePassword := "321password"
+
+	if user.CheckPassword(falsePassword) {
+		t.Errorf("False Password %v matched the correct Passord %v", falsePassword, plaintextPassword)
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(plaintextPassword))
+	if err != nil {
+		t.Errorf("Correct plaintext Password %v did not match: %v", plaintextPassword, err)
+	}
+
+}
